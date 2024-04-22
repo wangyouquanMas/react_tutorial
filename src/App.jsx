e71921eacb283d158c3db88cb28e5b78233b2329
Your Name <you@example.com>
2024-04-22 16:18:51 +0800

    card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
-import Food from './Food.jsx'
+import Card from './Card.jsx'
 
 
 function App() {
 
     return(
       <>
-        <Header></Header>
-        <Food/>
-        <Food/>
-        <Footer></Footer>
+      <Card/>
+      <Card/>
+      <Card/>
       </>
     );
 }
---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+
+function Card(){
+
+    return(
+        <div className="card">
+            <img className="card-image" src="https://via.placeholder.com/150" alt="profile picture"></img>
+            <h2 className="card-title">Joey</h2>
+            <p className="card-text">I design Products and make people happpy</p>
+        </div>
+    );
+}
+
+export default Card
