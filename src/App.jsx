d012ad72e93a7718d48781984b87a345edc52da4
Your Name <you@example.com>
2024-04-22 22:15:21 +0800

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import Button from './Button/Button.jsx'
+import Student from './Student.jsx'
 
 function App() {
 
     return(
-        <Button/>
+        <>
+          <Student name="Spongebob" age="30" isStudent={true}/>
+          <Student name="Patrick" age={42} isStudent={false}/>
+          <Student name="Squidward" age={50} isStudent={false}/>
+          <Student name="Sandy" age={27} isStudent={true}/>
+          <Student />
+          <Student name="Larry" />
+        </>
     );
 }
 
