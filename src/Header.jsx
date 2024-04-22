e383be14698ce6e9fa7f9384a39cfeedd2c0bd22
Your Name <you@example.com>
2024-04-22 15:40:32 +0800

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+
+function Header(){
+
+    return(
+        <header>
+            <h1>My website</h1>
+            <nva>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                </nva>
+                <hr></hr>
+        </header>
+    );
+}
+
+export default Header
